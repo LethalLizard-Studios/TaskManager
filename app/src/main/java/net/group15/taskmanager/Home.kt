64c22d7bde02b2403fdd5ec78508add5afbc9fa0
Hanwen Shi <hanwenshi@HanwendeMacBook-Pro.local>
2023-03-29 14:26:08 -0300

    add adaptor and layout file for home page

--- app/src/main/java/net/group15/taskmanager/Home.kt
+++ app/src/main/java/net/group15/taskmanager/Home.kt
@@ -7,11 +7,18 @@ import android.view.View
 import android.view.ViewGroup
 
 class Home : Fragment() {
+    val TAG:String = "TaskListActivity";
+
+    val dataList = mutableListOf<Task>()
+    val adapter = NoteAdapter(dataList)
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+
     }
+
 }
